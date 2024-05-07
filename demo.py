4850a07c9a4aeb05041eb2b2c51dab913e2a3a98
Aayush Pandey <97326217+aayushpandey8000@users.noreply.github.com>
2024-05-07 17:10:52 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -1,26 +1,37 @@
 import streamlit as st
-import base64
 
-def get_base64(png_file):
-    with open(png_file, "rb") as f:
-        return base64.b64encode(f.read()).decode()
+original_title = '<h1 style="font-family: serif; color:white; font-size: 20px;">Streamlit CSS Styling✨ </h1>'
+st.markdown(original_title, unsafe_allow_html=True)
 
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    .stApp {
-    background-image: url("data:image/jpeg;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
 
-def main():
-    set_background("D:\py_llm\law\magicpattern-mesh-gradient-1715052764545.png")
-    st.title("Streamlit App with Background Image")
-    st.write("This is a Streamlit app with a background image!")
+# Set the background image
+background_image = """
+<style>
+[data-testid="stAppViewContainer"] > .main {
+    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+    background-position: center;  
+    background-repeat: no-repeat;
+}
+</style>
+"""
 
-if __name__ == "__main__":
-    main()
+st.markdown(background_image, unsafe_allow_html=True)
+
+st.text_input("", placeholder="Streamlit CSS ")
+
+input_style = """
+<style>
+input[type="text"] {
+    background-color: transparent;
+    color: #a19eae;  // This changes the text color inside the input box
+}
+div[data-baseweb="base-input"] {
+    background-color: transparent !important;
+}
+[data-testid="stAppViewContainer"] {
+    background-color: transparent !important;
+}
+</style>
+"""
+st.markdown(input_style, unsafe_allow_html=True)
